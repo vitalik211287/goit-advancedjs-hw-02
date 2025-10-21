1faa2bb1f020714df17053da85384bb5c3595b78
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-10-21 21:03:28 +0300

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,43 @@
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const delay = Number(formEl.elements.delay.value);
+  const state = formEl.elements.state.value;
+
+  const shouldResolve = state === 'fulfilled';
+
+  createPromise(delay, shouldResolve)
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+
+  e.target.reset();
+
+  function createPromise(delay, shouldResolve) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve(delay); 
+        } else {
+          reject(delay); 
+        }
+      }, delay);
+    });
+  }
+}
